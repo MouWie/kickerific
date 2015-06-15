dfad617bc83d39ef4f1cc5fe91e8376c082c0da8
MouWie <mario.auernheimer@mantro.net>
2015-06-15 15:14:19 +0200

    Initial
    initial commit
    - created Project
    - inserted parse

---
+++ Kickerific/Kickerific/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Kickerific
+//
+//  Created by Mario on 15.06.15.
+//  Copyright (c) 2015 mantro.net. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
